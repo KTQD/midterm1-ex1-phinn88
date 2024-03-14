8921d7f7b5a6068a3ee051a03ea4332e1980e964
phinn88 <157661566+phinn88@users.noreply.github.com>
2024-03-14 08:23:52 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
+// File: Main.java
 public class Main {
     public static void main(String[] args) {
+        MP3 mp3 = new MP3("MP3 Player", "Portable music player", "MP3-001", 49.99f, 120);
+        Book book = new Book("Java Programming", "Learn Java programming basics", "BOOK-001", 29.99f, "John Doe", 300, "Programming");
 
-        MP3 mp3;
         mp3.showInfo();
-
-        Book book;
+        System.out.println();
         book.showInfo();
-
     }
 }
